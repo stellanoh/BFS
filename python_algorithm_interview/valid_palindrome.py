a7bbd648e6824f61073c4d8d3201230ebd223d62
henoh <henoh@nkia.co.kr>
2022-02-03 21:29:19 +0900

    leetcode #125 solved with deque

--- python_algorithm_interview/valid_palindrome.py
+++ python_algorithm_interview/valid_palindrome.py
@@ -1,4 +1,8 @@
-def isPalindrome(self, s):
+import collections
+from typing import Deque
+
+
+def is_palindrome_list(self, s: str) -> bool:
     # 리스트로 해결하는 방법
     strs = []
     # 영문자, 숫자만 리스트에 소문자로 변환 후 append
@@ -6,9 +10,23 @@ def isPalindrome(self, s):
         if char.isalnum():
             strs.append(char.lower())
 
-    # 팰린드롬 판별
+    # 팰린드롬 판별, O(N^2)
     while len(strs) > 1:
         if strs.pop(0) != strs.pop():
+            return False
+
+    return True
+
+
+def is_palindrome_deque(self, s: str) -> bool:
+    strs: Deque = collections.deque()
+
+    for char in s:
+        if char.isalnum():
+            strs.append(char.lower())
+    # O(N)
+    while len(strs) > 1:
+        if strs.popleft() != strs.pop():
             return False
 
     return True
